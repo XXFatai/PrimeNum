ec4c29cd728a6298cc4670269d7117577efa3327
XXFatai <trf669045207@126.com>
2013-01-25 18:47:24 +0800

    start

---
+++ src/com/trf/PrimeNum.java
@@ -0,0 +1,30 @@
+
+/**********************************************************
+ *  Copyright (C),2013, Hebei North University.
+ * PrjName：   PrimeNum
+ * FileName: PrimeNum.java
+ * Author:   润发
+ * Date:     2013-1-25 下午6:31:59  
+ * Version :
+ * Description:判断一个数是否为素数。
+ **********************************************************/
+package com.trf;
+import java.util.Scanner;
+
+public class PrimeNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+class Detemine{
+	public void dete(double n){
+		int i = 2;
+		
+		for (i = 2; i <= n; i++){
+			
+		}
+	}
+}
